5dd631a239a309f7c4265a275771b0d10f090dbd
Mihlali <biz@jordanmihlali.dev>
2022-03-01 22:58:24 +0200

    Configure toastify

--- app/root.tsx
+++ app/root.tsx
@@ -5,20 +5,22 @@ import {
   Outlet,
   Scripts,
   ScrollRestoration,
-} from "remix";
-import type { MetaFunction } from "remix";
+} from 'remix'
+import type { MetaFunction } from 'remix'
+import { ToastContainer } from 'react-toastify'
 import styles from './styles/app.css'
+import toastStyles from 'react-toastify/dist/ReactToastify.css'
 
 export function links() {
   return [
     { rel: 'stylesheet', href: styles },
-    // { rel: 'stylesheet', href: toastStyles },
+    { rel: 'stylesheet', href: toastStyles },
   ]
 }
 
 export const meta: MetaFunction = () => {
-  return { title: "New Remix App" };
-};
+  return { title: 'New Remix App' }
+}
 
 export default function App() {
   return (
@@ -30,11 +32,23 @@ export default function App() {
         <Links />
       </head>
       <body>
+        <ToastContainer
+          position="top-right"
+          autoClose={5000}
+          hideProgressBar={false}
+          newestOnTop={false}
+          closeOnClick
+          rtl={false}
+          pauseOnFocusLoss
+          draggable
+          pauseOnHover
+          theme="colored"
+        />
         <Outlet />
         <ScrollRestoration />
         <Scripts />
         <LiveReload />
       </body>
     </html>
-  );
+  )
 }
